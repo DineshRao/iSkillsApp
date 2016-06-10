ed926359e3615a9371715ca6686c5f3a92ef5922
mahendarkoppu <mahendarkoppu@gmail.com>
2016-06-09 19:57:29 -0500

    mahender home page ctrler modifoied

--- ISkillsMyApp/Controllers/HomePageController.cs
+++ ISkillsMyApp/Controllers/HomePageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ISkillsMyApp.Models;
 
 namespace ISkillsMyApp.Controllers
 {
@@ -11,7 +12,19 @@ namespace ISkillsMyApp.Controllers
         // GET: HomePage
         public ActionResult Index()
         {
-            return View();
+            ISkillsContext iSkillsContext = new ISkillsContext();
+            List<Categories> categories = iSkillsContext.Categories.ToList();
+            return View(categories);
+        }
+        public ActionResult CategoryProduct(int id)
+        {
+            ISkillsContext iSkillsContext = new ISkillsContext();
+            //  List<Categories> categories = iSkillsContext.Categories.ToList();
+
+            IEnumerable<Product> product = iSkillsContext.Products.Where(x => x.CategoryID == id);
+            //  if(product.)
+            return PartialView("_CategoryProduct", product);
+            // return View(product);
         }
     }
 }
